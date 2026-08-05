7b49352b195ac8711d8daff54627663bd46eed60
Belal Khaleel <119034702+BelalKhaleel@users.noreply.github.com>
2026-08-05 18:23:06 +0300

    feat: add input for quantity, increment and decrement buttons + add to cart button

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,38 @@
 import styles from "./Card.module.css";
+import { useState } from "react";
 
 const Card = ({
   imgUrl,
   altText,
   width = "100",
   height = "200",
+  item,
   category,
   title,
   description,
   rating,
   price,
+  onChange,
+  onAddToCartBtnClick,
 }) => {
+
+  const [value, setValue] = useState(0);
+
+  const handleDecrementValueClick = () => {
+    if (value > 0) {
+      const newValue = value - 1;
+      setValue(newValue);
+    }
+  }
+  
+  const handleIncrementValueClick = () => {
+    const newValue = value + 1;
+    setValue(newValue);
+  }
+
+  const handleAddToCartClick = () => {
+    onAddToCartBtnClick(value, item);
+  }
   return (
     <div className={styles.card}>
       <div className={styles.imgContainer}>
@@ -21,6 +43,28 @@ const Card = ({
       <p>{description}</p>
       <span>Rating: {rating}</span>
       <span>Price: {price}</span>
+      <div className={styles.quantity}>
+        <input
+          type="number"
+          min="0"
+          placeholder="0"
+          value={value}
+          onChange={onChange}
+        ></input>
+        <button type="button" onClick={handleDecrementValueClick}>
+          -
+        </button>
+        <button type="button" onClick={handleIncrementValueClick}>
+          +
+        </button>
+      </div>
+      <button
+        type="button"
+        className={styles.addToCartBtn}
+        onClick={handleAddToCartClick}
+      >
+        Add to cart
+      </button>
     </div>
   );
 };
